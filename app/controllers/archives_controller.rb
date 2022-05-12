e72275bd8185ea01fb36e5fe2f2519210b11b1f2
onomatopoetica <hi.jen@icloud.com>
2022-05-11 17:33:17 -0700

    Added all entries archives view by day

--- app/controllers/archives_controller.rb
+++ app/controllers/archives_controller.rb
@@ -1,5 +1,7 @@
 class ArchivesController < ApplicationController
   def index
-    @entries = Entry.all
+    @entries = Entry.all.group_by(&:day)
   end
 end
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
   get 'archives/index'
   resources :entries
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
   root to: "entries#index"
 end
